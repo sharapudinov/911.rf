133bb694778935d13e7dfbe1eca2b2b2603636da
artshar <sharapudinov@mial.ru>
2016-02-18 15:18:23 +0300

    landing

--- www/bitrix/templates/corp_services_blue/components/bitrix/catalog/911/bitrix/catalog.element/.default/component_epilog.php
+++ www/bitrix/templates/corp_services_blue/components/bitrix/catalog/911/bitrix/catalog.element/.default/component_epilog.php
@@ -4,18 +4,18 @@
 use Bitrix\Main\Loader;
 global $APPLICATION;?>
 <div class="container-fluid" id="location">
-	.<?$APPLICATION->IncludeComponent(
+	<?$APPLICATION->IncludeComponent(
 		"bitrix:map.google.view",
 		".default",
 		Array(
 			"COMPONENT_TEMPLATE" => ".default",
-			"CONTROLS" => array(0=>"SCALELINE",),
+			"CONTROLS" => array(0=>"SMALL_ZOOM_CONTROL",1=>"TYPECONTROL",2=>"SCALELINE",),
 			"INIT_MAP_TYPE" => "ROADMAP",
 			"MAP_DATA" => "a:4:{s:10:\"google_lat\";d:55.817302378730865;s:10:\"google_lon\";d:37.74755660871125;s:12:\"google_scale\";i:15;s:10:\"PLACEMARKS\";a:1:{i:0;a:3:{s:4:\"TEXT\";s:24:\"Автосервис 911\";s:3:\"LON\";d:37.75088596317801;s:3:\"LAT\";d:55.81923748074293;}}}",
 			"MAP_HEIGHT" => "500",
 			"MAP_ID" => "",
 			"MAP_WIDTH" => "100%",
-			"OPTIONS" => array(0=>"ENABLE_KEYBOARD",)
+			"OPTIONS" => array(0=>"ENABLE_SCROLL_ZOOM",1=>"ENABLE_DBLCLICK_ZOOM",2=>"ENABLE_DRAGGING",3=>"ENABLE_KEYBOARD",)
 		)
 	);?>
 	</div><?
