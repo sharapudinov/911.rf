c5ff9740b6d970b25b611b2fde7593e0da65587f
artshar <sharapudinov@mial.ru>
2016-02-18 15:05:14 +0300

    landing

--- www/bitrix/templates/corp_services_blue/components/bitrix/catalog/911/bitrix/catalog.element/.default/component_epilog.php
+++ www/bitrix/templates/corp_services_blue/components/bitrix/catalog/911/bitrix/catalog.element/.default/component_epilog.php
@@ -4,30 +4,20 @@
 use Bitrix\Main\Loader;
 global $APPLICATION;?>
 <div class="container-fluid" id="location">
-    <?$APPLICATION->IncludeComponent(
-	"bitrix:map.google.view",
-	".default",
-	array(
-		"COMPONENT_TEMPLATE" => ".default",
-		"INIT_MAP_TYPE" => "ROADMAP",
-		"MAP_DATA" => "a:4:{s:10:\"google_lat\";d:55.819707342668885;s:10:\"google_lon\";d:37.742743768310554;s:12:\"google_scale\";i:14;s:10:\"PLACEMARKS\";a:1:{i:0;a:3:{s:4:\"TEXT\";s:37:\"Магазин пиротехники\";s:3:\"LON\";d:37.751179933548;s:3:\"LAT\";d:55.819201918207;}}}",
-		"MAP_WIDTH" => "100%",
-		"MAP_HEIGHT" => "500",
-		"CONTROLS" => array(
-			0 => "SMALL_ZOOM_CONTROL",
-			1 => "TYPECONTROL",
-			2 => "SCALELINE",
-		),
-		"OPTIONS" => array(
-			0 => "ENABLE_SCROLL_ZOOM",
-			1 => "ENABLE_DBLCLICK_ZOOM",
-			2 => "ENABLE_DRAGGING",
-			3 => "ENABLE_KEYBOARD",
-		),
-		"MAP_ID" => ""
-	),
-	$component
-);?>
+	.<?$APPLICATION->IncludeComponent(
+		"bitrix:map.google.view",
+		".default",
+		Array(
+			"COMPONENT_TEMPLATE" => ".default",
+			"CONTROLS" => array(0=>"SCALELINE",),
+			"INIT_MAP_TYPE" => "ROADMAP",
+			"MAP_DATA" => "a:4:{s:10:\"google_lat\";d:55.817302378730865;s:10:\"google_lon\";d:37.74755660871125;s:12:\"google_scale\";i:15;s:10:\"PLACEMARKS\";a:1:{i:0;a:3:{s:4:\"TEXT\";s:24:\"Автосервис 911\";s:3:\"LON\";d:37.75088596317801;s:3:\"LAT\";d:55.81923748074293;}}}",
+			"MAP_HEIGHT" => "500",
+			"MAP_ID" => "",
+			"MAP_WIDTH" => "100%",
+			"OPTIONS" => array(0=>"ENABLE_KEYBOARD",)
+		)
+	);?>
 	</div><?
 if (isset($templateData['TEMPLATE_THEME']))
 {
