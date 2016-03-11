cfb883026273185318ee5d49049b3052beaaaf14
svoyn <svoynrav@yandex.ru>
2016-03-11 17:53:27 +0300

    Добавление Google Аналитики

--- www/bitrix/templates/corp_services_blue/footer.php
+++ www/bitrix/templates/corp_services_blue/footer.php
@@ -49,6 +49,7 @@
 	})();
 </script>
 <!-- End LeadBack code {/literal} -->
+
 <!-- Yandex.Metrika counter -->
 <script type="text/javascript">
 	(function (d, w, c) {
@@ -76,6 +77,17 @@
 </script>
 <noscript><div><img src="//mc.yandex.ru/watch/35440505" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
 <!-- /Yandex.Metrika counter -->
+
+<!-- Google Analitics -->
+<script>
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	ga('create', 'UA-73212558-3', 'auto');
+	ga('send', 'pageview');
+</script>
+<!-- End Google Analitics -->
 
 <!-- Google Tag Manager -->
 <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-N9ZHJW"
